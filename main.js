19aa6be4ec5f7860240c7d7b8fe2bda5538c5dbb
jeonnakwon <keemmone@gmail.com>
2020-08-20 22:34:34 +0900

    js : 키 이벤트 상태 체크하기

--- main.js
+++ main.js
@@ -3,24 +3,28 @@ const startBtn = document.querySelector('.startBtn');
 const gameArea = document.querySelector('.gameArea');
 const gameMessage = document.querySelector('.gameMessage');
 
-
+//키보드 감지처리 
 document.addEventListener('keydown', pressOn);
 document.addEventListener('keyup', pressOff);
-
 //game start 기능
 startBtn.addEventListener('click', start);
 gameMessage.addEventListener('click', start);
+
+let keys = { };  
 
 function start(){
     gameMessage.classList.add('hide');
     startBtn.classList.add('hide');
 }
 
-//키보드 감지처리 
-function pressOn(){
-    console.log('pressOn');
+function pressOn(e){
+    console.log(e.code);
+    keys[e.code] = true;
+    console.log(keys);
 }
 
-function pressOff(){
-    console.log('pressOff');
+function pressOff(e){
+    console.log(e.code);
+    keys[e.code] = false;
+    console.log(keys);
 }
